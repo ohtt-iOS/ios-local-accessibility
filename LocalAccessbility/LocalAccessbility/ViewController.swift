5d2e7136cc56661eaa69d26c76b270cc46dd8ab6
yagom <yagom@yagom.net>
2021-03-25 13:50:26 +0900

    initial commit

---
+++ LocalAccessbility/LocalAccessbility/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LocalAccessbility - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private func addBottomLabel() {
+        // Do any additional setup after loading the view.
+        
+        let bottomLabel = UILabel()
+        bottomLabel.frame = CGRect(x: 20, y: view.bounds.size.height - 150, width: view.bounds.size.width - 40, height: 50)
+        
+        bottomLabel.text = "Touch the button below!"
+        
+        bottomLabel.font = UIFont.systemFont(ofSize: 20)
+        
+        bottomLabel.textColor = .red
+        
+        bottomLabel.textAlignment = .center
+        
+        view.addSubview(bottomLabel)
+    }
+    private func addButton() {
+        // Do any additional setup after loading the view.
+        
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 20, y: view.bounds.size.height - 80, width: view.bounds.size.width - 40, height: 50)
+        
+        button.setTitle("Touch Me!", for: .normal)
+        
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        button.setTitleColor(.blue, for: .normal)
+        
+        view.addSubview(button)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addBottomLabel()
+        addButton()
+        
+    }
+
+
+}
+
